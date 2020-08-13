c239f1e7df2ff8f25d972378f54aa95a63fc8033
20171308046 <monastars458@qq.com>
2020-08-13 21:04:26 +0800

    桥接模式ing

---
+++ GitUnityTest/Assets/Scripts/GameFunction/InteractSystem/I_Object_Interact.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 互动游戏物体接口
+/// </summary>
+public abstract class I_Object_Interact
+{ 
+    //需要控制的各种组件
+    private I_TipUI_Interact m_TipUI = null;
+    protected GameObject m_InteractObject = null;
+    protected MeshRenderer m_MeshRend = null;
+
+    public void SetTipUI(I_TipUI_Interact TipUI)
+    {
+        if (m_TipUI != null)
+            m_TipUI.Release();
+        m_TipUI = TipUI;
+
+        //设定UI拥有者
+        m_TipUI.SetOwner(this);
+
+    }
+
+    public I_TipUI_Interact GetTipUI()
+    {
+        return m_TipUI;
+    }
+
+    public void SetMeshRender()
+    {
+        m_MeshRend = UnityTool.FindChildGameObjec
+    }
+
+    #region 交互物体操作
+    /// <summary>
+    /// 进行绑定UI预制体销毁
+    /// </summary>
+    public void Release()
+    {
+        if (m_InteractObject != null)
+            GameObject.Destroy(m_InteractObject);
+    }
+
+    /// <summary>
+    /// 设定绑定的UI
+    /// </summary>
+    /// <param name="theUICompent"></param>
+    public void SetGameObject(GameObject theInteractObject)
+    {
+        m_InteractObject = theInteractObject;
+    }
+
+    /// <summary>
+    /// 得到绑定的UI
+    /// </summary>
+    /// <returns></returns>
+    public GameObject GetObjectCompent()
+    {
+        return m_InteractObject;
+    }
+
+    #endregion
+}
---
+++ GitUnityTest/Assets/Scripts/GameFunction/InteractSystem/RayserUIsystem.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayserUIsystem : BaseManeger<RayserUIsystem>
+{
+    private Camera _cameraUI;
+    private InteractSlot tipUI ;
+    //游戏物体与UI坐标交互的层级
+    private LayerMask _interactionMask;
+    private LayerMask _movementMask;
+    //捕获到的临时游戏物体
+    private GameObject _wallobject;
+
+    /// <summary>
+    /// 初始化系统并且负责注册有关事件
+    /// </summary>
+    public void InitRayserSystem(Camera camera)
+    {
+        _interactionMask = LayerMask.GetMask("Interactable");
+        _movementMask = LayerMask.GetMask("Ground");
+        _cameraUI = camera;
+
+        EventCenter.GetInstance().AddEventListener("CatchTarget",CatchTarget);
+        EventCenter.GetInstance().AddEventListener("MoveTarget",MoveTarget);
+        EventCenter.GetInstance().AddEventListener("CreateTarget", CreateTarget);
+    }
+
+    /// <summary>
+    /// 射线获取游戏物体
+    /// </summary>
+    private void CatchTarget()
+    {
+        // 射出一条射线
+        Ray ray = _cameraUI.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // 如果射线与物体交互
+        if (Physics.Raycast(ray, out hit, _interactionMask) )
+        {
+            //隐藏获得的目标物体
+            Debug.Log("捕捉物体" + hit.collider.name + " " + hit.point);
+            //获取物体信息
+            _wallobject = hit.transform.GetComponent<GameObject>();
+            //隐藏物体
+            _wallobject.GetComponent<InteractObject>().HideAndShowSelf();
+        }
+    }
+
+
+    /// <summary>
+    /// 射线检测不断移动物体
+    /// </summary>
+    private void MoveTarget()
+    {
+        // 射出一条射线
+        Ray ray = _cameraUI.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        
+        // 检测射线在Ground标签上的位置
+        if (Physics.Raycast(ray, out hit, _movementMask))
+        {
+        
+            Debug.Log("重新设置物体：" + hit.point);
+            //改变物体方位
+            _wallobject.transform.position = hit.point;
+            //更新UI的位置
+            _wallobject.GetComponent<InteractObject>().TipUI.GetComponent<InteractSlot>().FollowTheSlot();
+            //显示物体
+            _wallobject.GetComponent<InteractObject>().HideAndShowSelf();
+        }
+    }
+
+    /// <summary>
+    /// 射线产生新物体
+    /// </summary>
+    private void CreateTarget()
+    {
+        // 射出一条射线
+        Ray ray = _cameraUI.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // 检测射线在Ground标签上的位置
+        if (Physics.Raycast(ray, out hit, _movementMask))
+        {
+
+            Debug.Log("创建新物体：" + hit.point);
+            //得到新的物体
+            _wallobject = ResourcesMgr.GetInstance().Load<GameObject>("Prefabs/WallObject");
+            //调整物体方位
+            _wallobject.transform.position = hit.point;
+            //初始化物体
+            _wallobject.GetComponent<InteractObject>().InitObject();
+        }
+    }
+
+
+}
